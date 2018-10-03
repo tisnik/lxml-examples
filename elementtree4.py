3003f6ae32641808da2abd630c98f110438311b3
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-10-03 10:13:30 +0200

    4th example: more complex ElementTree

---
+++ elementtree4.py
@@ -0,0 +1,27 @@
+import lxml.etree as ET
+
+root = ET.Element("root")
+
+left1stLevel = ET.SubElement(root, "left")
+middle1stLevel = ET.SubElement(root, "middle")
+right1stLevel = ET.SubElement(root, "right")
+
+left2ndLevelA = ET.SubElement(left1stLevel, "left")
+middle2ndLevelA = ET.SubElement(left1stLevel, "middle")
+right2ndLevelA = ET.SubElement(left1stLevel, "right")
+
+left2ndLevelB = ET.SubElement(middle1stLevel, "left")
+middle2ndLevelB = ET.SubElement(middle1stLevel, "middle")
+right2ndLevelB = ET.SubElement(middle1stLevel, "right")
+
+left2ndLevelC = ET.SubElement(right1stLevel, "left")
+middle2ndLevelC = ET.SubElement(right1stLevel, "middle")
+right2ndLevelC = ET.SubElement(right1stLevel, "right")
+
+# konstrukce stromu
+tree = ET.ElementTree(root)
+
+# zapis do souboru
+tree.write("test4.xml")
+
+tree.write("test4_pretty_print.xml", pretty_print=True)
