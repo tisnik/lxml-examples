2b0eb0ad129aed64da9b0f39a6ebe9f932fa8ceb
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-10-11 08:36:01 +0200

    pom.xml parser #6

---
+++ read_pom6.py
@@ -0,0 +1,11 @@
+import lxml.etree as ET
+
+xml = "pom.xml"
+tree = ET.parse(xml)
+
+root = tree.getroot()
+
+dependency_ids = tree.xpath('/project/dependencies/dependency/groupId[.="junit"]/text()')
+
+for dependency_id in dependency_ids:
+    print(dependency_id)
